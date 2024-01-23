41e49a3cf1f531c60086eed558993d5d135be9b4
mattglbrt <matt@everyway.io>
2024-01-23 10:24:36 -0500

    Netlify forms

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -6,24 +6,21 @@ export function SignUpForm() {
   let id = useId()
 
   return (
-    <form name="email signup" data-netlify="true"
-      method="POST"
-
-      className="relative isolate mt-8 flex items-center pr-1">
+    <form className="relative isolate mt-8 flex items-center pr-1" data-netlify="true">
       <label htmlFor={id} className="sr-only">
         Email address
       </label>
       <input
         required
         type="email"
-        autoComplete="Email"
-        name="Email"
+        autoComplete="email"
+        name="email"
         id={id}
         placeholder="Email address"
         className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
       />
       <Button type="submit" arrow>
-        Grow With Me
+        Get updates
       </Button>
       <div className="absolute inset-0 -z-10 rounded-lg transition peer-focus:ring-4 peer-focus:ring-sky-300/15" />
       <div className="absolute inset-0 -z-10 rounded-lg bg-white/2.5 ring-1 ring-white/15 transition peer-focus:ring-sky-300" />
