19c6f9be2b7bf52f4c574316fc8de794d51c1aa1
mattglbrt <matt@everyway.io>
2024-03-12 11:30:30 -0400

    Moved to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+  site: 'https://lexingtonthemes.com',
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
