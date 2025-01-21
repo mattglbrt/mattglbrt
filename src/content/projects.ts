ce769a9a8ad09d99b133c7c0bdc65b0342646fb4
mattglbrt <matt@everyway.io>
2025-01-21 14:14:41 -0500

    photos updated.

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -6,7 +6,7 @@ export const PROJECTS = [
     title: "Everyway Design",
     description:
       "I don't just build some of the fastest websites you have ever seen. I provide the systems and tech behind them to support your business and achieve your goals. If you want to hire me, this is the place to go.",
-    image: "/neovim.jpg",
+    image: "/everywaydesign.jpg",
     tags: ["Wordpress", "Shopify", "Optimization", "Lead Gen", "AI"],
     links: [
       {
@@ -19,7 +19,7 @@ export const PROJECTS = [
   {
     title: "The Hobbinomicon",
     description: "A community, podcast, and website devoted to TTRPGs, Miniature Painting, and Table Top Wargames, with a focus on indie games and helping people actually play them.",
-    image: "/markwriter.jpg",
+    image: "/hobbinomicon.jpg",
     tags: [],
     links: [
       {
@@ -42,7 +42,7 @@ export const PROJECTS = [
   {
     title: "The Book of Valtiare",
     description: "I don't entirely know what this is going to be yet, but if you like the Hobbinomicon and the things I talk about there, hopefully you will like this too.",
-    image: "/blueriver.jpg",
+    image: "/valtiare.jpg",
     tags: [],
     links: [
       {
