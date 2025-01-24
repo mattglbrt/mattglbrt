de68f187a049fba2fb68e9ecf83255dc477bdf20
mattglbrt <matt@everyway.io>
2025-01-23 19:12:25 -0500

    Content updates

--- src/content/experiences.ts
+++ src/content/experiences.ts
@@ -1,7 +1,7 @@
 export const EXPERIENCES = [
   {
     date: "January 2025",
-    jobTitle: "Spiraling Upward",
+    jobTitle: "Starting Fresh",
     company: "Chesapeake, VA",
     description:
       "I've been here many times before, but this time feels different. This time I building in public and doing my best to share everything I know with the world. Living with my wife, 4 kids, 2 dogs and a cat.",
